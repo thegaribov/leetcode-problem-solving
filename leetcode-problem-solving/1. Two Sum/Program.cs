9df0f3918fa70f19eb4dad189d49722d940a4b97
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-04-09 15:42:45 +0400

    Two sum problem completed

---
+++ leetcode-problem-solving/1. Two Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1._Two_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int y = i + 1; y < nums.Length; y++)
+                {
+                    if (nums[i] + nums[y] == target)
+                    {
+                        return new int[] { i, y };
+                    }
+                }
+            }
+
+            return new int[] { };
+        }
+    }
+}
